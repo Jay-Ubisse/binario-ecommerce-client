3435db8184c49b3cd71db41212857f5d17681b52
Jay-Ubisse <joaquimubisse@gmail.com>
2024-03-05 15:23:07 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Binario Store",
+  description:
+    "Loja de venda de material de escritório e e aparelhos electrónicos.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
